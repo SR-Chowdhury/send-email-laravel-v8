f1be53e5165f12dd4c2ded7837494d18bed6b9c0
SR-Chowdhury <hackedgdown@gmail.com>
2021-05-05 21:07:45 +0600

    Email send in Laravel 8 done

--- resources/views/dynamic_email_template.blade.php
+++ resources/views/dynamic_email_template.blade.php
@@ -1,4 +1,20 @@
-<h3><b>Email: {{ $data['email'] }}</b></h3>
-<p>Name: {{ $data['name'] }}</p>
-<p>Message: {{ $data['message'] }}</p><br>
-<p>It would be appriciative, if you gone through this feedback.</p>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mail From SWMC-Client</title>
+</head>
+
+<body>
+
+    <h3><b>Email: {{ $data['email'] }}</b></h3>
+    <p>Name: {{ $data['name'] }}</p>
+    <p>Message: {{ $data['message'] }}</p><br>
+    <p>It would be appriciative, if you gone through this feedback.</p>
+
+</body>
+
+</html>
